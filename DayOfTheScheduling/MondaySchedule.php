655c564b2ffaceb1e03d6b8a63a4c204a41ca381
MisterMunchkin <MisterMunchkin@users.noreply.github.com>
2017-09-15 21:56:46 +0800

    new update flow, optimization

--- DayOfTheScheduling/MondaySchedule.php
+++ DayOfTheScheduling/MondaySchedule.php
@@ -50,26 +50,19 @@
   // if negative then late
   if(mysqli_num_rows($monthlyResult) > 0){
     //user table exist just update existing
-    $monthlyRows = mysqli_fetch_array($monthlyResult);
+  /*  $monthlyRows = mysqli_fetch_array($monthlyResult);
     $updatedTotalLate = new DateInterval('PT0H0M');// = strtotime("0:00:00");
     $updatedTotalOvertime = new DateInterval('PT0H0M'); //= strtotime("0:00:00");
     $updatedTotalHours = new DateInterval('PT0H0M'); //= strtotime("0:00:00");
 
     if($timeLate != "0:00:00"){
-      //$monthlyRows["TotalLate"] += $timeLate;
-    //  $updatedTotalLate = (String) strtotime($monthlyRows["TotalLate"]) + strtotime($timeLate);
-        $updatedTotalLate = $intervalLate;//strtotime($timeLate);
+        $updatedTotalLate = $intervalLate
     }
     if($timeOvertime != "0:00:00"){
-      //$updatedTotalOvertime = (String) strtotime($monthlyRows["TotalOvertime"]) +
-        //    strtotime($timeOvertime);
-        $updatedTotalOvertime = $intervalOvertime;//strtotime($timeOvertime);
-        //echo "$updatedTotalOvertime";
+        $updatedTotalOvertime = $intervalOvertime;
     }
-    //$temp = $monthlyRows["TotalOvertime"];
-    //echo "'$updatedTotalOvertime'.'$temp'";
     $updatedTotalHours = $interval;
-    //$updatedTotalOvertime += strtotime($monthlyRows["TotalOvertime"]);
+
     $OvertimeArray = array(
       "hours" => substr($monthlyRows["TotalOvertime"],0,1),
       "minutes" => substr($monthlyRows["TotalOvertime"],2,2)
@@ -89,8 +82,7 @@
                                         $LateArray["minutes"].'M');
     $oldTotalHours = new DateInterval('PT'.$TotalHoursArray["hours"].'H'.
                                           $TotalHoursArray["minutes"].'M');
-    //echo "'$updatedTotalOvertime'";
-    //$updatedTotalOvertime = date
+
     $FinalOvertime = new DateTime("0:00:00");
     $FinalTotalHours = new DateTime("0:00:00");
     $FinalLate = new DateTime("0:00:00");
@@ -104,73 +96,60 @@
     $FinalLate->add($oldLate);
     $FinalLateInterval = $FinalLate->add($updatedTotalLate);
 
-  //  echo $FinalLateInterval->format("h:i:s")." ".$FinalTotalHoursInterval->format("h:i:s");
-/*
-    $stringformatLate = sprintf(
-      '%d:%02d:%02d',
-      $FinalLateInterval->h,
-      $FinalLateInterval->i,
-      $FinalLateInterval->s
-    );
-    $stringformatOvertime = sprintf(
-      '%d:%02d:%02d',
-      $FinalOvertimeInterval->h,
-      $FinalOvertimeInterval->i,
-      $FinalOvertimeInterval->s
-    );
-    $stringformatTotalHours = sprintf(
-      '%d:%02d:%02d',
-      $FinalTotalHoursInterval->h,
-      $FinalTotalHoursInterval->i,
-      $FinalTotalHoursInterval->s
-    );*/
-    /*
-    $stringformatLate = sprintf(
-      '%d:%02d:%02d',
-      ($updatedTotalLate->d * 24) + $updatedTotalLate->h,
-      $updatedTotalLate->i,
-      $updatedTotalLate->s
-    );
-    $stringformatOvertime = sprintf(
-      '%d:%02d:%02d',
-      ($updatedTotalOvertime->d * 24) + $updatedTotalOvertime->h,
-      $updatedTotalOvertime->i,
-      $updatedTotalOvertime->s
-    );
-    $stringformatTotalHours = sprintf(
-      '%d:%02d:%02d',
-      ($updatedTotalHours->d *24) + $updatedTotalHours->h,
-      $updatedTotalHours->i,
-      $updatedTotalHours->s
-    );*/
-  //  $stringformatLate = date('H:i:s', $updatedTotalLate);
-  //  $stringformatOvertime = date('H:i:s', $updatedTotalOvertime);
-  //  $stringTotalHours = date('H:i:s', $updatedTotalHours);
-  $stringformatLate = $FinalLateInterval->format("h:i:s");
-  $stringformatTotalHours = $FinalTotalHoursInterval->format("h:i:s");
-  $stringformatOvertime = $FinalOvertimeInterval->format("h:i:s");
 
-  $updateMonthlysql = "UPDATE `totalhourspermonth`
-  SET `TotalLate`='$stringformatLate',`TotalHours`='$stringformatTotalHours',
-  `TotalOvertime`='$stringformatOvertime'
-   WHERE `userID` = '$userID'";
-/*
+    $stringformatLate = $FinalLateInterval->format("h:i:s");
+    $stringformatTotalHours = $FinalTotalHoursInterval->format("h:i:s");
+    $stringformatOvertime = $FinalOvertimeInterval->format("h:i:s");
+
     $updateMonthlysql = "UPDATE `totalhourspermonth`
     SET `TotalLate`='$stringformatLate',`TotalHours`='$stringformatTotalHours',
     `TotalOvertime`='$stringformatOvertime'
-     WHERE `userID` = '$userID'";*/
+     WHERE `userID` = '$userID'";
 
      if(mysqli_query($conn,$updateMonthlysql)){
        echo "monday update success";
      }else{
        echo "monday update fail";
      }
+*/
+    $row = mysqli_fetch_array($monthlyResult);
 
-  }else{// insert works, but update doesnt yet
-    //insert new table since no table exist
-    //$formatTimeLate = strtotime($timeLate);
-    //$formatOvertime = strtotime($timeOvertime);
-    //$formatTotalHours = strtotime($time);
+    $monthlyLate = new DateTime($row["TotalLate"]);
+    $monthlyOvertime = new DateTime($row["TotalOvertime"]);
+    $monthlyTotalHours = new DateTime($row["TotalHours"]);
+
+    $interval = $monthlyTotalHours->add($interval);
+
+    $TotalHoursString = sprintf(
+      '%d:%02d:%02d',
+      ($interval->d * 24) + $interval->h,
+      $interval->i,
+      $interval->s
+    );
+
+    $intervalLate = $monthlyLate->add($intervalLate);
+
+    $TotalLateString = sprintf(
+      '%d:%02d:%02d',
+      ($intervalLate->d * 24) + $intervalLate->h,
+      $intervalLate->i,
+      $intervalLate->s
+    );
+
+    $intervalOvertime = $monthlyOvertime->add($intervalOvertime);
+
+    $TotalOvertimeString = sprintf(
+      '%d:%02d:%02d',
+      ($intervalOvertime->d * 24) + $intervalOvertime->h,
+      $intervalOvertime->i,
+      $intervalOvertime->s
+    );
+
+    $updateMonthlysql = "UPDATE `totalhourspermonth`
+    SET `TotalLate`='$TotalLateString',`TotalHours`='$TotalHoursString',
+    `TotalOvertime`='$TotalOvertimeString'
+     WHERE `userID` = '$userID'";
+  }else{
 
     $insertMonthlysql = "INSERT INTO `totalhourspermonth`(`TotalLate`,
        `TotalHours`, `TotalOvertime`, `userID`)
